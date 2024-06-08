5a8fbea8ba3f80a1034b9aeed060337c8a9abb14
parvvaresh <parvvaresh@gmail.com>
2024-06-08 10:45:18 +0330

    add decision tree pharameter

---
+++ parameter/decision_tree.py
@@ -0,0 +1,9 @@
+param_dt = {
+    'criterion': ['gini', 'entropy'],
+    'splitter': ['best', 'random'],
+    'max_depth': [None, 5, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'random_state': [42],
+}
