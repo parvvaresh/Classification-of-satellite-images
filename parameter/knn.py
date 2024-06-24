fd5069c436da38c8fab15dd2815e70c1f502e9a9
parvvaresh <parvvaresh@gmail.com>
2024-06-24 18:48:58 +0330

    add pharameter for each models

--- parameter/knn.py
+++ parameter/knn.py
@@ -1,6 +1,5 @@
 param_knn = {
-    'n_neighbors': list(range(3, 15)),  # Number of neighbor
-    'weights': ['uniform', 'distance'],  # Weight function
-    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],  # Algorithm to compute neighbors
-    'metric': ['euclidean', 'manhattan', 'chebyshev']  # Distance metric
+    'n_neighbors': list(range(1, 15, 2)),
+    'weights': ['uniform', 'distance'],
+    'metric': ['euclidean', 'manhattan', 'minkowski']
 }
--- parameter/perceptron.py
+++ parameter/perceptron.py
@@ -1,7 +1,5 @@
 param_perceptron = {
-    'penalty': ['l2', 'l1', 'elasticnet'],
-    'alpha_l1': [0.0001, 0.001, 0.01, 0.1],  # Alpha for l1 penalty
-    'alpha_l2': [0.0001, 0.001, 0.01, 0.1],  # Alpha for l2 penalty
-    'learning_rate': ['constant', 'optimal', 'adaptive'],
-    'eta0': [0.01, 0.1, 0.5]  # Initial learning rate
+    'penalty': ['l1', 'l2', 'elasticnet'],
+    'alpha': [0.0001, 0.001, 0.01, 0.1, 1],
+    'max_iter': [1000, 2000, 3000]
 }
