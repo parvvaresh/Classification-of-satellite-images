9393ff0bfa716026b06f72a218da8d4c1cc17d5b
parvvaresh <parvvaresh@gmail.com>
2024-06-24 19:02:25 +0330

    fix select model : add new metric

--- select_model.py
+++ select_model.py
@@ -1,50 +1,87 @@
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.linear_model import Perceptron
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import (
+    recall_score,
+    accuracy_score,
+    precision_score,
+    f1_score,
+    confusion_matrix,
+    cohen_kappa_score,
+    make_scorer
+)
 
-def select_best_model(X_train, y_train, models):
+import numpy as np
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-    best_model = None
-    best_params = None
-    best_score = 0
-    metrics = {}
-    
-    for model_name, (model, param_grid) in models.items():
-        grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=-1)
-        grid_search.fit(X_train, y_train)
-        
-        metrics[model_name] = {}
-        
-        for params, mean_score, scores in zip(
-            grid_search.cv_results_['params'],
-            grid_search.cv_results_['mean_test_score'],
-            grid_search.cv_results_['std_test_score']
-        ):
-            model.set_params(**params)
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_train)
-            
-            accuracy = accuracy_score(y_train, y_pred)
-            precision = precision_score(y_train, y_pred)
-            recall = recall_score(y_train, y_pred)
-            f1 = f1_score(y_train, y_pred)
-            
-            metrics[model_name][str(params)] = {
-                'mean_score': mean_score,
-                'std_score': scores,
-                'accuracy': accuracy,
-                'precision': precision,
-                'recall': recall,
-                'f1_score': f1
-            }
-            
-            if mean_score > best_score:
-                best_model = model_name
-                best_params = params
-                best_score = mean_score
-    
-    return best_model, best_params, best_score, metrics
+import warnings
+warnings.filterwarnings("ignore")
 
 
+
+
+
+
+def classification_parameter_finder(model,
+                                    parameters : dict,
+                                    X_train : np.array,
+                                    y_train : np.array,
+                                    X_test : np.array,
+                                    y_test : np.array):
+    start = time.time()
+
+    # Create a scorer based on Cohen's kappa
+    kappa_scorer = make_scorer(cohen_kappa_score)
+
+    # Set GridSearchCV to optimize for kappa
+    grid = GridSearchCV(model, param_grid=parameters, refit=True, cv=5, n_jobs=-1, scoring=kappa_scorer)
+    grid.fit(X_train, y_train)
+
+    y_train_pred = grid.predict(X_train)
+    y_test_pred = grid.predict(X_test)
+
+    train_accuracy = accuracy_score(y_train, y_train_pred)
+    test_accuracy = accuracy_score(y_test, y_test_pred)
+
+    precision = precision_score(y_test, y_test_pred, average='weighted')
+    recall = recall_score(y_test, y_test_pred, average='weighted')
+    f1 = f1_score(y_test, y_test_pred, average='weighted')
+    kappa = cohen_kappa_score(y_test, y_test_pred)
+
+    conf_matrix = confusion_matrix(y_test, y_test_pred)
+    class_labels = np.unique(y_test)
+
+    model_name = str(model).split('(')[0]
+
+    end = time.time()
+
+    results = pd.DataFrame({
+        "model": [model_name],
+        "best_params": [grid.best_params_],
+        "train_accuracy": [train_accuracy],
+        "test_accuracy": [test_accuracy],
+        "precision": [precision],
+        "recall": [recall],
+        "f1_score": [f1],
+        "kappa": [kappa],
+        "confusion_matrix": [conf_matrix.ravel()],
+        "runtime": [end - start]
+    })
+
+    name = f"{model_name}.csv"
+    results.to_csv(name)
+
+    print(f"The best parameters for {model_name} model is: {grid.best_params_}")
+    print("--" * 10)
+    print(f"Accuracy in the training set is {train_accuracy:0.2%} for {model_name} model.")
+    print(f"Accuracy in the testing set is {test_accuracy:0.2%} for {model_name} model.")
+    print(f"Precision is {precision:0.2%} for {model_name} model.")
+    print(f"Recall is {recall:0.2%} for {model_name} model.")
+    print(f"F1 Score is {f1:0.2%} for {model_name} model.")
+    print(f"Kappa Score is {kappa:0.2%} for {model_name} model.")
+    print("--" * 10)
+    print(f"Runtime of the program is: {end - start:0.2f} seconds")
+
+    # Plot the confusion matrix
+    #plot_confusion_matrix(conf_matrix, class_labels, model_name)
