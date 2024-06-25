48d8916ec1cb1809f2395571389b99598daaae63
parvvaresh <parvvaresh@gmail.com>
2024-06-25 17:30:42 +0330

    add pre-process | split s1 and s2 for pca and ...

---
+++ pre_process/utils.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from data.parser import get_spectrums
+
+def split_data(df: pd.DataFrame) -> dict:
+    s1_columns = []
+    s2_columns = []
+
+    for column in df.columns:
+        if extract_spectrum(column).upper() in data["s1"]:
+            s1_columns.append(column)
+        elif extract_spectrum(column).upper() in data["s2"]:
+            s2_columns.append(column)
+
+    s1 = pd.concat([df[col] for col in s1_columns], axis=1)
+    s2 = pd.concat([df[col] for col in s2_columns], axis=1)
+
+    return {
+        "s1": s1,
+        "s2": s2
+    }
+
+def extract_spectrum(column: str) -> str:
+    spectrum = ""
+
+    try:
+        _, spectrum = column.split("_")
+    except ValueError:
+        return spectrum
+
+    return spectrum
