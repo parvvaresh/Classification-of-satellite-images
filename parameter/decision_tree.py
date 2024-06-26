ef100f59dee960e58c5e9fc29c0f4559ca4b5336
parvvaresh <parvvaresh@gmail.com>
2024-06-26 08:12:35 +0330

    add pharameter : native bayes

--- parameter/decision_tree.py
+++ parameter/decision_tree.py
@@ -1,3 +1,15 @@
+"""
+ explain :
+        --> criterion :
+                pass
+        --> max_depth :
+                pass
+        --> min_samples_split :
+                pass
+        --> min_samples_leaf :
+                pass
+"""
+
 param_decisionTree = {
     'criterion': ['gini', 'entropy'],
     'max_depth': [None, 10, 20, 30, 40],
---
+++ parameter/naive_bayes.py
@@ -0,0 +1,3 @@
+param_naive_bayes = {
+    'var_smoothing': [1e-9, 1e-8, 1e-7, 1e-6, 1e-5]
+}
