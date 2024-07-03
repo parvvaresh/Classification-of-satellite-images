c598a5a39e0c286ad184f21d8896ad214ab58f09
parvvaresh <parvvaresh@gmail.com>
2024-07-03 11:49:58 +0330

    return df for split s1 & s2

--- classification/pre_process/standardize.py
+++ classification/pre_process/standardize.py
@@ -1,4 +1,4 @@
-import numpy as np
+import pandas as pd
 from sklearn.preprocessing import (
                                     StandardScaler,
                                     MinMaxScaler,
@@ -8,24 +8,40 @@ from sklearn.preprocessing import (
 
 
 
-def standardize(X : np.array) -> list:
+def standardize(df : pd.array) -> list:
+
+
+  columns = df.columns
+
+
   scaler_standard = StandardScaler()
-  X_standard_scaled = scaler_standard.fit_transform(X)
+  X_standard_scaled = scaler_standard.fit_transform(df)
+  df_standard_scaled = pd.DataFrame(X_standard_scaled, columns=columns)
+
 
   scaler_minmax = MinMaxScaler()
-  X_minmax_scaled = scaler_minmax.fit_transform(X)
+  X_minmax_scaled = scaler_minmax.fit_transform(df)
+  df_minmax_scaled = pd.DataFrame(X_minmax_scaled, columns=columns)
+
 
   scaler_maxabs = MaxAbsScaler()
-  X_maxabs_scaled = scaler_maxabs.fit_transform(X)
+  X_maxabs_scaled = scaler_maxabs.fit_transform(df)
+  df_maxabs_scaled = pd.DataFrame(X_maxabs_scaled, columns=columns)
+
+
 
   scaler_robust = RobustScaler()
-  X_robust_scaled = scaler_robust.fit_transform(X)
+  X_robust_scaled = scaler_robust.fit_transform(df)
+  df_robust_scaled = pd.DataFrame(X_robust_scaled, columns=columns)
+
 
   scaler_normalizer = Normalizer()
-  X_normalized = scaler_normalizer.fit_transform(X)
+  X_normalized = scaler_normalizer.fit_transform(df)
+  df_normalized = pd.DataFrame(X_normalized, columns=columns)
 
-  return [X_standard_scaled,
-          X_minmax_scaled,
-          X_maxabs_scaled,
-          X_robust_scaled,
-          X_normalized]
+  return [
+          df_standard_scaled,
+          df_minmax_scaled,
+          df_maxabs_scaled,
+          df_robust_scaled,
+          df_normalized]
