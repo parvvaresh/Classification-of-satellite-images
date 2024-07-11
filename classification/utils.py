5974583f00ffb171059a0fcf5db32d9dcfcf8472
reza <parvvaresh@gmail.com>
2024-07-11 15:13:59 +0330

    add split data sets

--- classification/utils.py
+++ classification/utils.py
@@ -1,73 +1,33 @@
-from pre_process.utils import split_data
-from pre_process.standardize import standardize
-from pre_process.dimensionality_reduction.lda import lda_split, lda
-from pre_process.dimensionality_reduction.lda import pca_split, pca
-
 import pandas as pd
 import numpy as np
+from pre_process.data.parser import get_spectrums
 
+def split_data(df: pd.DataFrame) -> list:
+    s1_columns = []
+    s2_columns = []
 
+    data = get_spectrums()
 
+    for column in df.columns:
+        if extract_spectrum(column).upper() in data["s1"]:
+            s1_columns.append(column)
+        elif extract_spectrum(column).upper() in data["s2"]:
+            s2_columns.append(column)
 
+    s1 = pd.concat([df[col] for col in s1_columns], axis=1)
+    s2 = pd.concat([df[col] for col in s2_columns], axis=1)
 
+    return [
+            np.array(s1),
+            np.array(s2)
+        ]
 
-def _split_satellite(data : pd.DataFrame) -> dict:
-  s1 , s2 = split_data(data)
-  return {
-      "orinal" : data,
-      "s1" : s1,
-      "s2" : s2
-    }
+def extract_spectrum(column: str) -> str:
+    spectrum = ""
 
+    try:
+        _, spectrum = column.split("_")
+    except ValueError:
+        return spectrum
 
-def _standardize_data(data  : dict) -> dict:
-
-  result = {}
-
-  for name, _data in data.items():
-    X_standard_scaled, X_minmax_scaled, X_maxabs_scaled, X_robust_scaled, X_normalized = standardize(_data)
-    result[name] = {
-        "orginal" : _data,
-        "standard_scaled" : X_standard_scaled,
-        "minmax_scaled" : X_minmax_scaled,
-        "maxabs_scaled" : X_maxabs_scaled,
-        "robust_scaled" : X_robust_scaled,
-        "normalized" : X_normalized
-      }
-  return result
-
-
-
-def _dimensionality_reduction(data : dict,
-                              y : dict) -> dict:
-  result = {}
-
-  for  name, _data_dict in data.items():
-    result_temp_pca= {}
-    result_temp_lda= {}
-
-    if name == "orginal":
-      for _name , _data in _data_dict.items():
-        _result_pca = pca(_data, None)
-        result_temp_pca[_name] = _result_pca
-
-        _result_lda = lda(_data, y, None)
-        result_temp_lda[_name] = _result_lda
-
-      result[f"{name}-PCA"] =  result_temp_pca
-      result[f"{name}-LDA"] = result_temp_lda
-
-    else:
-
-      for _name , _data in _data_dict.items():
-        _result_pca = pca_split(_data)
-        result_temp_pca[_name] = _result_pca
-
-        _result_lda = lda_split(_data, y)
-        result_temp_lda[_name] = _result_lda
-
-      result[f"{name}-PCA"] =  result_temp_pca
-      result[f"{name}-LDA"] = result_temp_lda
-
-
-
+    return spectrum
