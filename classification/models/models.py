67200c3d8c1423f5dea9f6cedde73baccb2ae9f4
reza <parvvaresh@gmail.com>
2024-07-11 16:00:38 +0330

    fix folder for return model and pharameter

---
+++ classification/models/models.py
@@ -0,0 +1,21 @@
+from decision_tree import get_dt
+from knn import get_knn
+from logstic_regression import get_lr
+from mlp import get_mlp
+from naive_bayes import get_nb
+from perceptron import get_pr
+from random_forest import get_rf
+from svm import get_svm
+
+
+def get_details_models():
+    return [
+        get_knn(),
+        get_dt(),
+        get_lr(),
+        get_mlp(),
+        get_nb(),
+        get_pr(),
+        get_rf(),
+        get_svm()
+    ]
---
+++ classification/models/svm.py
@@ -0,0 +1,12 @@
+from sklearn.svm import SVC
+
+def get_svm():
+    param_svm = {
+        'C': [0.1, 1, 10, 100, 1000],
+        'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+        'degree': [2, 3, 4],  # Degree for 'poly' kernel
+        'gamma': ['scale', 'auto', 0.001, 0.01, 0.1, 1],
+        'coef0': [0, 0.1, 0.5, 1],  # Coef0 for 'poly' and 'sigmoid' kernels
+    }
+
+    return SVC(), param_svm
