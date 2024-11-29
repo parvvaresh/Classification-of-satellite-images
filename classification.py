8f67996db7dbdc4e9a2b8585ce6cc29924cd890d
parvvaresh <parvvaresh@gmail.com>
2024-11-29 11:35:31 +0330

    fix bugs

--- classification.py
+++ classification.py
@@ -4,11 +4,13 @@ from train_models import train_models
 
 
 def classification(df : pd.DataFrame,
-         class_column : str) -> None:
+                    class_column : str,
+                    path : str,
+                    name : str) -> None:
     
     x_data , y = pre_process(df, class_column)
 
-    train_models(x_data, y)
+    train_models(x_data, y, path , name)
 
 
 
